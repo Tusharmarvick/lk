bf5cc43e27316b8249c2194b30f25525d01e1536
marvick <tushargijomarvick@gmail.com>
2023-02-03 17:10:42 +0530

    moving

---
+++ Sample4.java
@@ -0,0 +1,14 @@
+class Sample4
+{
+public static void main(String[] args)
+{
+int b=13;
+int c=b+'A';
+b=c-'a';
+System.out.println("b");
+int a='b'+'c';
+System.out.println(a);
+a=b+c;
+System.out.print("a");
+}
+}
